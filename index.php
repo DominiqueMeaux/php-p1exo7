81ef48e97d5e6a8fbcc9e7d74bfad37a33b7ce42
dominiquem <dominique.meaux@gmail.com>
2018-07-26 09:27:54 +0200

    php p1exo7

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>p1exo7</title>
+  </head>
+  <body>
+    <p>
+      Bonjour
+      <?php
+        $lastname = 'Meaux ';
+          echo $lastname;
+        $firstname = 'Dominique ';
+          echo $firstname;
+      ?>
+      ,tu as
+      <?php
+        $age = 34 ;
+          echo $age;
+      ?>
+      ans.
+    </p>
+  </body>
+</html>
